2f9fe41635a22f40c2fdd028dfab9adb901efd77
RCasares316 <richiecasares@gmail.com>
2026-02-09 13:05:33 -0500

    hoots create form almost done

--- src/components/HootCreate/HootCreate.jsx
+++ src/components/HootCreate/HootCreate.jsx
@@ -1,7 +1,66 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+
 const HootCreate = (props) => {
+  const [formData, setFormData] = useState({
+    title: "",
+    text: "",
+    category: "News",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
+//   const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("formData", formData);
+  };
+
   return (
     <main>
-      <h1>Hoot Create</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title-input">Title</label>
+        <input
+          required
+          type="text"
+          name="title"
+          id="title-input"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <label htmlFor="text-input">Text</label>
+        <textarea
+          required
+          type="text"
+          name="text"
+          id="text-input"
+          value={formData.text}
+          onChange={handleChange}
+        />
+        <label htmlFor="category-input">Category</label>
+        <select
+          required
+          name="category"
+          id="category-input"
+          value={formData.category}
+          onChange={handleChange}
+        >
+          <option value="News">News</option>
+          <option value="Games">Games</option>
+          <option value="Music">Music</option>
+          <option value="Movies">Movies</option>
+          <option value="Sports">Sports</option>
+          <option value="Television">Television</option>
+        </select>
+        <button type="submit">SUBMIT</button>
+      </form>
     </main>
   );
 };
