4108c60fa986439d92d0895a1143cb6bbd5afce6
RCasares316 <richiecasares@gmail.com>
2026-02-09 10:46:05 -0500

    HootCreate set up

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={user ? <Dashboard /> : <Landing />} />
         <Route path="/hoots" element={<HootList />} />
+        <Route path="/hoots/new" element={<h1>New Hoot</h1>} />
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/sign-in" element={<SignInForm />} />
       </Routes>
